3568cc12fd7c9c8250ddd48927c3b0629667c08b
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2025-01-19 19:47:35 -0800

    In class work

--- src/CharacterFrequencyCounterTest.java
+++ src/CharacterFrequencyCounterTest.java
@@ -3,5 +3,23 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CharacterFrequencyCounterTest {
-    
+    @Test
+    public void testLAppearsThreeTimesInHelloWorld() {
+        //Arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("Hello World");
+        //Act
+        int actualCount = counter.getFrequency('l');
+        //Assert
+        assertEquals(3, actualCount);
+    }
+
+    @Test
+    public void testNonOccurringCharacterPercentageIsZero() {
+        //arrange
+        CharacterFrequencyCounter counter = new CharacterFrequencyCounter("Hi There");
+        //Act
+        double actualPercentage = counter.getRelativePercentage('z');
+        //Assert
+        assertEquals(0.0, actualPercentage, 0.0001);
+    }
 }
